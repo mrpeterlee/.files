2b0aaa0133f9855753a2959b74e843098dba1f89
Peter Lee <peter.lee@finclab.com>
2022-04-28 04:16:25 +0000

    added spelling dict and code skeleton

---
+++ neovim/skeleton/skeleton.py
@@ -0,0 +1,28 @@
+""" [TODO:ModuleName]
+id:            Peter Lee (peter.lee@finclab.com)
+last_update:   [TODO:LastUpdate]
+type:          lib
+sensitivity:   datalab@finclab.com
+platform:      any
+description:   [TODO:Description]
+"""
+
+from pprint import pprint
+from typing import Dict, List, Optional, Union
+
+import numpy as np
+import pandas as pd
+from finclab.logger import init_logger
+from munch import Munch
+
+logger = init_logger()
+
+
+def main() -> None:
+    """[DocString]"""
+    return None
+
+
+if __name__ == "__main__":
+    main()
+    logger.info("[TODO:ModuleName] is finished.")
