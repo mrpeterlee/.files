b1ee9ce8bd27d1bb78ef29a356392d84f17322c6
Peter Lee <peter.lee@astrocapital.net>
2023-01-19 14:33:30 -0500

    working

--- windows/bin/RDP-Winclient.bat
+++ windows/bin/RDP-Winclient.bat
@@ -1,35 +1,36 @@
 @echo off
-cls
 
 taskkill /f /im "mstsc.exe"
 
+timeout 1
 
 set ip="10.1.1.81"
 echo Connect to Windows Client (%ip%) with resolution of 1920x1080
-call :ConnectToWindowsClient
+call :ConnectToWindowsClient %ip%
 
 set ip="10.1.1.82"
 echo Connect to Windows Client (%ip%) with resolution of 1920x1080
-call :ConnectToWindowsClient
+call :ConnectToWindowsClient %ip%
 
 set ip="10.1.1.83"
 echo Connect to Windows Client (%ip%) with resolution of 1920x1080
-call :ConnectToWindowsClient
+call :ConnectToWindowsClient %ip%
 
 set ip="10.1.1.84"
 echo Connect to Windows Client (%ip%) with resolution of 1920x1080
-call :ConnectToWindowsClient
+call :ConnectToWindowsClient %ip%
 
 exit
 
 :ConnectToWindowsClient
+
+    set ip="%~1"
     ping -n 1 %ip% | find "TTL"
     if not errorlevel 1 set result=SUCCESS
     if errorlevel 1 set result=FAIL
-    cls
 
     if "%result%" == "SUCCESS" (
-        start "" mstsc /v:10.1.1.81 /w:1920 /h:1080 /span
+        start "" mstsc /v:%ip% /w:1920 /h:1080 /span
     )
 
 exit /b
