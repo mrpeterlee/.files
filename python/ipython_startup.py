9d253f24fbd93f760fea28cebbfe5878423282c4
Peter Lee <peter.lee@astrocapital.net>
2024-04-12 16:49:29 -0400

    added alias for ipython

---
+++ python/ipython_startup.py
@@ -0,0 +1,10 @@
+from pprint import pprint
+
+import finclab
+import numpy as np
+import pandas as pd
+from finclab.logger import init_logger
+from finclab.util.dataframe import *
+from finclab.util.dt import *
+
+logger = init_logger()
